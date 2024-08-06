5e3101a5470eb13fa1db70be392080d5e8a0958a
Thor 🪁 <7041313+thor314@users.noreply.github.com>
2024-08-06 21:56:22 +0100

    chore: clean up the repo (#18)
    * .github ci
    
    * readme updated
    
    * justfile
    
    * fmt
    
    * lints
    
    * move aes circuits -> circuits/aes directory
    
    * fmt settings
    
    * main refactor
    
    * refactor: main -> lib. mv Witness -> witness mod
    
    * mv witness type aliases to crate root
    
    * witness refactor
    
    * witness minor refactor notes
    
    * refactor proof
    
    * just adjust

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+//! This generates witnesses to test circom artifacts in the `circuits` directory.
+
+#![feature(trivial_bounds)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unreachable_code)]
+#![allow(non_snake_case)]
+#![allow(clippy::clone_on_copy)]
+#![allow(unused_mut)]
+
+use std::io;
+
+use aes::{cipher::generic_array::GenericArray, Aes256};
+use ark_circom::CircomBuilder;
+use ark_ec::pairing::Pairing;
+use cipher::consts::U16;
+
+mod consts;
+mod proof;
+mod utils;
+mod witness;
+
+/// Circom compilation artifacts
+/// Must compile circom artifacts first if these aren't found.
+const SIV_WTNS: &str = "./build/gcm_siv_dec_2_keys_test_js/gcm_siv_dec_2_keys_test.wasm";
+const SIV_R1CS: &str = "./build/gcm_siv_dec_2_keys_test.r1cs";
+const AES_256_CRT_WTNS: &str = "./build/aes_256_ctr_test_js/aes_256_ctr_test.wasm";
+const AES_256_CRT_R1CS: &str = "./build/aes_256_ctr_test.r1cs";
+
+pub type AAD = [u8; 5];
+pub type Nonce = [u8; 12];
+
+// convenience type aliases for AES-CTR, wrapping type aliases from `ctr` crate
+pub(crate) type Ctr32BE<Aes128> = ctr::CtrCore<Aes128, ctr::flavors::Ctr32BE>;
+pub(crate) type Aes256Ctr32BE = ctr::Ctr32BE<Aes256>;
+pub(crate) type Aes128Ctr32BE = ctr::Ctr32BE<aes::Aes128>; // Note: Ctr32BE is used in AES GCM
+
+/// AES 128-bit block
+pub(crate) type Block = GenericArray<u8, U16>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Test the AES-GCM-SIV circuit (from electron labs)
+    #[tokio::test]
+    async fn test_aes_gcm_siv() -> io::Result<()> {
+        // generate witness
+        let mut witness = witness::aes_witnesses(witness::CipherMode::GcmSiv).unwrap();
+
+        // log one of them
+        println!(
+            "proof gen: key={:?}, iv={:?}, ct={:?}, pt={:?}",
+            witness.key, witness.iv, witness.ct, witness.pt
+        );
+
+        // TODO(TK 2024-08-06): replace hackz with documented methods
+        //  hackz for 128 bit iv, Ask Tracy about this
+        witness.iv.extend_from_slice(&[0, 0, 0, 0]);
+
+        // generate proof
+        proof::gen_proof_aes_gcm_siv(&witness, SIV_WTNS, SIV_R1CS);
+        Ok(())
+    }
+}
---
+++ src/utils.rs
@@ -0,0 +1,66 @@
+use ark_bn254::Fr;
+use ark_circom::CircomBuilder;
+use ark_ec::pairing::Pairing;
+
+use crate::{Nonce, AAD};
+
+// TODO(TK 2024-08-06): test with test vectors at bottom of rfc 8452
+// @devloper: do you know/couldyou find where make_nonce is specified in rfc8452?
+//
+/// construct the nonce from the `iv` and `seq` as specified in RFC 8452
+/// https://www.rfc-editor.org/rfc/rfc8452
+pub(crate) fn make_nonce(iv: [u8; 12], seq: u64) -> Nonce {
+    let mut nonce = [0u8; 12];
+    nonce[4..].copy_from_slice(&seq.to_be_bytes());
+
+    nonce.iter_mut().zip(iv).for_each(|(nonce, iv)| {
+        *nonce ^= iv;
+    });
+
+    nonce
+}
+
+pub(crate) fn make_tls13_aad(len: usize) -> AAD {
+    [
+        0x17, // ContentType::ApplicationData
+        0x3,  // ProtocolVersion (major)
+        0x3,  // ProtocolVersion (minor)
+        (len >> 8) as u8,
+        len as u8,
+    ]
+}
+
+// TODO(TK 2024-08-06): @devloper, document and refactor for transparency
+pub(crate) fn push_bytes_as_bits<T: Pairing>(
+    mut builder: CircomBuilder<T>,
+    field: &str,
+    bytes: &[u8],
+) -> CircomBuilder<T> {
+    for byte in bytes {
+        for i in 0..8 {
+            let bit = (byte >> (7 - i)) & 1;
+            builder.push_input(field, bit as u64);
+        }
+    }
+
+    builder
+}
+
+// convert bits to bytes
+pub(crate) fn bits_to_u8(bits: &[u8]) -> u8 {
+    bits.iter().rev().enumerate().fold(0, |acc, (i, &bit)| acc | ((bit & 1) << i))
+}
+
+pub(crate) fn parse_bit_from_field(j: &Fr) -> u8 {
+    // TODO(TK 2024-08-06): move to lazy static to avoid duplication
+    let ONE = Fr::from(1);
+    let ZERO = Fr::from(0);
+
+    if *j == ONE {
+        1u8
+    } else if *j == ZERO {
+        0u8
+    } else {
+        panic!("results should be bits")
+    }
+}
