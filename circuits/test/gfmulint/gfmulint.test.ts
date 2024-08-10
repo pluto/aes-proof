24a44e7c994db1bb78d4d170f3a607b00f0bc6b7
Thor Kampefner <thorck@pm.me>
2024-08-10 19:09:28 -0400

    minor adjustumnts to gfmultest

--- circuits/test/gfmulint/gfmulint.test.ts
+++ circuits/test/gfmulint/gfmulint.test.ts
@@ -1,5 +1,6 @@
 import { WitnessTester } from "circomkit";
 import { circomkit } from "../common";
+import { assert } from "chai";
 
 describe("gfmulint", () => {
   let circuit: WitnessTester<["a", "b"], ["res"]>;
@@ -28,13 +29,15 @@ describe("gfmulint", () => {
     // b = 2048
     // a*b = 2,097,152
 
-    var a = 1;
-    var b = 2;
-    var zero = 0;
+    const a = 1;
+    const b = 1;
+    const zero = 0;
+    const expected = a*b;
 
-    const zero_bits = zero.toString(2).padStart(64, '0').split('').map(bit => parseInt(bit, 10));
-    const a_bits = a.toString(2).padStart(64, '0').split('').map(bit => parseInt(bit, 10));
-    const b_bits = b.toString(2).padStart(64, '0').split('').map(bit => parseInt(bit, 10));
+    // little endian pad a, b, zero
+    const zero_bits = pad_to_64_bits(zero);
+    const a_bits = pad_to_64_bits(a);
+    const b_bits = pad_to_64_bits(b);
     
     let input = {a: [zero_bits, a_bits], b: [zero_bits, b_bits]};
     let wit = await circuit.calculateWitness(input);
@@ -42,10 +45,21 @@ describe("gfmulint", () => {
     // circuit.expectPass(input, {res: })
     // let res = await circuit.expectConstraintPass(wit);
 
-    let res = await circuit.compute(input, ["res"])
-    let first_64: number[] = (res.res as number[][])[0];
-    let last_64: number[] = (res.res as number[][])[1]
-    let all_bits: number[] = first_64.concat(last_64);
-    console.log("output:", parseInt(all_bits.join(''), 2));
+    let _res = await circuit.compute(input, ["res"])
+    let result = parse_res(_res.res as number[][]);
+    assert.equal(result, expected);
+    console.log(`${a} x ${b} = ${result}`);
   });
 });
+
+function pad_to_64_bits(value: number): number[] {
+  return value.toString(2).padStart(64, '0').split('').map(bit => parseInt(bit, 10));
+}
+
+// write a helper function to parse res
+function parse_res(res: number[][]): number {
+  let first_64: number[] = res[0];
+  let last_64: number[] = res[1];
+  let all_bits: number[] = first_64.concat(last_64);
+  return parseInt(all_bits.join(''), 2);
+}
