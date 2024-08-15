ef54fc838e88ea9618e60d7b069102302269726f
Waylon Jepsen <waylonjepsen1@gmail.com>
2024-08-15 08:39:27 -0700

    comments

--- circuits/test/gfmulx.test.ts
+++ circuits/test/gfmulx.test.ts
@@ -17,11 +17,10 @@ describe("GFMulX", () => {
       console.log("#constraints:", await circuit.getConstraintCount());
     });
 
-    // x^8 + x^4 + x^3 + x + 1
     it("should compute correctly 1", async () => {
         // x^128 = x^7 + x^2 + x + 1
         let test = [1].concat(Array(127).fill(0)) // 128-bit array with MSB = 1 and everything else 0
-        /// 10000111
+        /// 10000111 : x^7 + x^2 + x + 1
         let expected_output: number[] = Array(120).fill(0).concat([1,0,0,0,0,1,1,1])
         await circuit.expectPass({in: test}, {out: expected_output})
     });
