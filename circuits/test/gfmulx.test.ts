3a4f7818a5c0a600295a35df5d674b8ac744a3a9
Waylon Jepsen <waylonjepsen1@gmail.com>
2024-08-15 08:38:19 -0700

    RFC test vector fails

--- circuits/test/gfmulx.test.ts
+++ circuits/test/gfmulx.test.ts
@@ -25,5 +25,27 @@ describe("GFMulX", () => {
         let expected_output: number[] = Array(120).fill(0).concat([1,0,0,0,0,1,1,1])
         await circuit.expectPass({in: test}, {out: expected_output})
     });
+
+    it("should compute correctly 2", async () => {
+        /// test vector rfc 8452 appendix A.1 https://datatracker.ietf.org/doc/html/rfc8452#appendix-A
+        let bits = hexToBits("01000000000000000000000000000000")
+        let expected_output = hexToBits("00800000000000000000000000000000")
+        console.log(bits);
+        console.log(expected_output);
+        await circuit.expectPass({in: bits}, {out: expected_output})
+    });
   });
 });
+
+function hexToBits(hex: string): number[] {
+    // Remove the '0x' prefix if present
+    if (hex.startsWith('0x')) {
+      hex = hex.slice(2);
+    }
+  
+    // Convert hex to binary string
+    const binaryString = BigInt(`0x${hex}`).toString(2).padStart(hex.length * 4, '0');
+  
+    // Convert binary string to array of bits
+    return binaryString.split('').map(bit => parseInt(bit, 10));
+  }
