79ebf64b556f2a800cd3fe29770274efa963d393
Waylon Jepsen <waylonjepsen1@gmail.com>
2024-09-09 10:59:48 -0600

    invSBox + Tests

--- circuits/test/sbox128.test.ts
+++ circuits/test/sbox128.test.ts
@@ -18,4 +18,20 @@ describe("SBox128", () => {
       await circuit.expectPass({ in: 0x00 }, { out: 0x63 });
     });
   });
+
+
+  describe("InvSBox128", () => {
+    before(async () => {
+      circuit = await circomkit.WitnessTester(`InvSubBytes`, {
+        file: "aes-ctr/sbox128",
+        template: "InvSBox128",
+      });
+      console.log("#constraints:", await circuit.getConstraintCount());
+    });
+
+    it("should compute correctly", async () => {
+      await circuit.expectPass({ in: 0xed }, { out: 0x53 });
+      await circuit.expectPass({ in: 0x63 }, { out: 0x00 });
+    });
+  });
 });
