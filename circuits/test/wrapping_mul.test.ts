0fb4b233046c4b181f93bfcd67bfd91677da2079
KaiGeffen <geffenkai@gmail.com>
2024-09-12 17:41:31 -0400

    Added wrap_mul test, fixed minor scope smell

--- circuits/test/wrapping_mul.test.ts
+++ circuits/test/wrapping_mul.test.ts
@@ -15,7 +15,7 @@ describe("WRAPPING_BE", () => {
   it("wrapping mul low values", async () => {
     const a = 2;
     const a_arr = padArrayTo64Bits(numberToBitArray(a));
-    for (var b = 1; b < 16; b++) {
+    for (let b = 1; b < 16; b++) {
       const expected = numberTo16Hex(a * b);
       const _res = await circuit.compute({ a: a_arr, b: hexToBitArray(numberTo16Hex(b)) }, ["out"]);
       const result = bitArrayToHex(
@@ -69,6 +69,32 @@ describe("WRAPPING_BE", () => {
     const a = hexToBitArray("0xFFFFFFFFFFFFFFFF");
     const b = hexToBitArray("0xFFFFFFFFFFFFFFFF");
     const expected = "0000000000000001";
+
+    const _res = await circuit.compute({ a, b }, ["out"]);
+    const result = bitArrayToHex(
+      (_res.out as (number | bigint)[]).map((bit) => Number(bit))
+    );
+
+    assert.equal(result, expected, "Wrap-around on overflow is incorrect");
+  });
+
+  it("should correctly wrap on maximum overflow", async () => {
+    const a = hexToBitArray("0xFFFFFFFFFFFFFFFF");
+    const b = hexToBitArray("0xFFFFFFFFFFFFFFFF");
+    const expected = "0000000000000001";
+
+    const _res = await circuit.compute({ a, b }, ["out"]);
+    const result = bitArrayToHex(
+      (_res.out as (number | bigint)[]).map((bit) => Number(bit))
+    );
+
+    assert.equal(result, expected, "Wrap-around on overflow is incorrect");
+  });
+
+  it("should correctly wrap on large numbers below max", async () => {
+    const a = hexToBitArray("0xa5a5a5a5a5a5a5a5");
+    const b = hexToBitArray("0x5a5a5a5a5a5a5a5a");
+    const expected = "a76b2ef2b67a3e02";
 
     const _res = await circuit.compute({ a, b }, ["out"]);
     const result = bitArrayToHex(
