8e1864d1e19ad17e89d9fe1f842be1c2e589f3bc
Waylon Jepsen <waylonjepsen1@gmail.com>
2024-09-19 15:52:39 -0600

    tracies test vector passing

--- src/main.rs
+++ src/main.rs
@@ -107,7 +107,7 @@ mod tests {
         };
         use hex_literal::hex;
 
-        // first byte is 00000001
+        // first bit is 1
         const H: [u8; 16] =   hex!("80000000000000000000000000000000");
         const X: [u8; 16] = hex!("80000000000000000000000000000000");
 
@@ -116,15 +116,26 @@ mod tests {
         ghash.update(&[X.into()]);
         let result = ghash.finalize();
 
+        // last bit is 1
         const H_1: [u8; 16] = hex!("00000000000000000000000000000001");
         const X_1: [u8; 16] = hex!("00000000000000000000000000000001");
-        // Alternative.                  
+       
         let mut ghash2 = GHash::new(&H_1.into());
         ghash2.update(&[X_1.into()]);
         let result2 = ghash2.finalize();
 
-        println!("GHASH result_1: {:?}", hex::encode(result.as_slice()));
-        println!("GHASH result_2: {:?}", hex::encode(result2.as_slice()));
+        // test vector of pain
+        const H_2: [u8; 16] = hex!("aae06992acbf52a3e8f4a96ec9300bd7");
+        const X_2: [u8; 16] = hex!("98e7247c07f0fe411c267e4384b0f600");
+        
+        let mut ghash3 = GHash::new(&H_2.into());
+        ghash3.update(&[X_2.into()]);
+        let result3 = ghash3.finalize();
+
+        println!("GHASH Test vector 1: {:?}", hex::encode(result.as_slice()));
+        println!("GHASH Test vector 2: {:?}", hex::encode(result2.as_slice()));
+        println!("GHASH Test vector 3: {:?}", hex::encode(result3.as_slice()));
+
         // println!("expected: {:?}", hex::encode(expected));
 
     }
