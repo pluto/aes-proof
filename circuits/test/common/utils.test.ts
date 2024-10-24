41d3a52ff009b776730414b0dc8754d0f328f20d
Waylon Jepsen <waylonjepsen1@gmail.com>
2024-10-23 19:55:15 -0600

    remove unused test

--- circuits/test/common/utils.test.ts
+++ circuits/test/common/utils.test.ts
@@ -73,47 +73,6 @@ describe("IncrementWord", () => {
 });
 
 
-describe("ParseBytesBE", () => {
-  let circuit: WitnessTester<["in"], ["out"]>;
-
-  before(async () => {
-    circuit = await circomkit.WitnessTester(`ParseBEBytes64`, {
-      file: "aes-gcm/utils",
-      template: "ParseBEBytes64",
-    });
-  });
-
-  it("Should parse bytes in BE order", async () => {
-    const X = hexToBitArray("0x0000000000000001");
-    const expected = 1;
-    const _result = await circuit.compute({ in: X }, ["out"]);
-    const result = _result.out as number;
-
-    assert.equal(result, expected, "parse incorrect");
-  });
-});
-
-
-describe("ParseBytesLE", () => {
-  let circuit: WitnessTester<["in"], ["out"]>;
-
-  before(async () => {
-    circuit = await circomkit.WitnessTester(`ParseLEBytes64`, {
-      file: "aes-gcm/utils",
-      template: "ParseLEBytes64",
-    });
-  });
-
-  it("Should parse bytes in LE order", async () => {
-    const X = hexToBitArray("0x0100000000000000");
-    const expected = 1;
-    const _result = await circuit.compute({ in: X }, ["out"]);
-    const result = _result.out as number;
-
-    assert.equal(result, expected, "parse incorrect");
-  });
-});
-
 describe("ArrayMux", () => {
   let circuit: WitnessTester<["a", "b", "sel"], ["out"]>;
 
